668c8ff5b38c67fd6a205370e84570d3e1f041ea
jensensboefhus <andreas.sg@live.dk>
2026-06-11 14:05:40 +0200

    pid control code

--- pid.py
+++ pid.py
@@ -7,6 +7,22 @@ sample_input = [16, 17, 18, 19, 18, 18, 19, 17, 18, 19, 17, 18, 18, 19, 18, 19, 
 kp, ki, kd = 1, 0.5, 0.5
 
 
+def mussel_system():
+    intT = 0.0
+    errorT = 0.0
+    intOD = 0.0
+    errorOD = 0.0
+    while(True):
+        intT, errorT, control_signal_temp, temp = pid_temp(intT, errorT)
+        cooler_pump(control_signal_temp)
+
+        intOD, errorOD, control_signal_OD, od = pid_od(intOD, errorOD)
+        algae_pump(control_signal_OD)
+
+        serverUpload(temp, control_signal_temp, od, control_signal_OD)
+        wait(1)
+
+
 def pid_temp(integral, prev_err):
     temp = read_temperature()
     error = ref_temp - temp
@@ -26,17 +42,3 @@ def pid_od(integral, prev_err):
     dterm = kd * (error - prev_err)
     sum = pterm + dterm + iterm
     return integral, error, sum, od
-
-def musselSystem():
-    intT = 0.0
-    errorT = 0.0
-    intOD = 0.0
-    errorOD = 0.0
-    while(True):
-        intT, errorT, control_signal_temp, temp = pid_temp(intT, errorT)
-        cooler_pump(control_signal_temp)
-        intOD, errorOD, control_signal_OD, od = pid_od(intOD, errorOD)
-        algae_pump(control_signal_OD)
-
-        serverUpload(temp, control_signal_temp, od, control_signal_OD)
-        wait(1)
